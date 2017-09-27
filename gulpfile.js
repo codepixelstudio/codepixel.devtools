7befe8cfbe194c44ded7665c6afd4dd575321d8c
Code Pixel Studio <codepixel@codepixelstudio.com>
2017-09-27 10:49:47 -0600

    preliminary SCSS architecture rebuild
    fixes styles broken in Chrome 60.0.xxxx

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,103 @@
 // configure gulp tasks
 
-    // variables
-        var gulp = require( 'gulp' );
-        var sass = require( 'gulp-sass' );
+    // global task variables
+        var gulp       = require( 'gulp' );
+        var sass       = require( 'gulp-sass' );
+        var notify     = require( 'gulp-notify' );
+        var rename     = require( 'gulp-rename' );
+        var concat     = require( 'gulp-concat' );
+        var path       = require( 'path' );
+        var dateformat = require( 'dateformat' );
+        var sourcemaps = require( 'gulp-sourcemaps' );
+        var uglify     = require( 'gulp-uglify' );
+
+    // other variables
+        var basedate = new Date();
+        var timestamp = dateformat( basedate, 'yyyy.mm.dd hh.MM' );
+        notifycon = function( task ) {
+
+            return './assets/img/notifycon_' + task + '.png';
+
+        };
+
+        gulp.Gulp.prototype.__runTask = gulp.Gulp.prototype._runTask;
+        gulp.Gulp.prototype._runTask = function( task ) {
+
+            this.currentTask = task;
+            this.__runTask(task);
+
+        }
+
+    // javascript task variables
+        var jspath = './inc/js/build/**/*.js';
 
     // tasks
 
         // stylesheet
-            gulp.task( 'stylesheet', function() {
+            gulp.task( 'stylesheets', function() {
 
-                gulp.src( 'assets/scss/**/*.scss' )
+                gulp.src( 'assets/scss/codepixel.devtools.scss' )
 
                     .pipe( sass().on( 'error', sass.logError ) )
 
-                    .pipe( gulp.dest( './assets/css/' ) );
+                    .pipe( gulp.dest( './assets/css/' ) )
+
+                    .pipe( notify({
+
+                        title: 'compiled',
+                        message: '<%= file.relative %> : <%= options.timestamp %>',
+                        onLast: true,
+                        icon: path.join( __dirname, notifycon( this.currentTask.name ) ),
+                        templateOptions: {
+
+                            timestamp: timestamp
+
+                        }
+
+                    }) );
+
+            });
+
+        // javascript
+            gulp.task( 'javascript', function() {
+
+                return gulp.src( jspath )
+
+                    .pipe( sourcemaps.init() )
+
+                    .pipe( concat( 'state.gameday.js' ) )
+
+                    .pipe( gulp.dest( './inc/js/build/' ) )
+
+                    .pipe( rename( 'state.gameday.min.js' ) )
+
+                    .pipe( uglify() )
+
+                    .pipe( sourcemaps.write( './' ) )
+
+                    .pipe( gulp.dest( '.inc/js/build/' ) )
+
+                    .pipe( notify({
+
+                        title: 'compiled',
+                        message: '<%= file.relative %> : <%= options.timestamp %>',
+                        onLast: true,
+                        icon: path.join( __dirname, notifycon( this.currentTask.name ) ),
+                        templateOptions: {
+
+                            timestamp: timestamp
+
+                        }
+
+                    }) );
 
             });
 
         // watch
-            gulp.task( 'default', function() {
+            gulp.task( 'watch', function() {
 
-                gulp.watch( 'assets/scss/**/*.scss', [ 'stylesheet' ] );
+                gulp.watch( 'assets/scss/**/*.scss', [ 'stylesheets' ] );
 
             });
+
+            gulp.task( 'default', [ 'stylesheets', 'javascript', 'watch' ] );
